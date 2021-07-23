b74d62bc1d1ff4553e37217b858ac0cb1cfc70cf
Mtuozzo86 <mtuozzo86@gmail.com>
2021-07-23 17:24:26 -0400

    commit tree d8aabcb730d72fdff63c66de3a2d27a82ff09bff

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Your code here
+// function saturdayFun(activity="roller-skate"){
+//     return `This Saturday, I want to ${activity}!`
+// }
+// saturdayFun("bathe my dog")
+
+// const mondayWork = function (activity="go to the office"){
+//     return `This Monday, I will ${activity}.`
+// }
+
+// function wrapAdjective(character="*"){
+//     const outcome = wrapAdjective(string="a hard worker")
+// }   return outcome = `You are ${character}${string}${character}!`
+
+function saturdayFun(activity = "roller-skate" ){
+    return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(activity = "go to the office"){
+    return `This Monday, I will ${activity}.`
+}
